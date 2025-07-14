15d826da0b4489ff58a939e96906073b9f35085a
SantiagoGJ <santigutierrez0001@gmail.com>
2025-07-13 20:05:45 -0500

    Se cambia nombre de archivo main y se agrega las lineas para importar librerias de gson

---
+++ src/main/java/RastreadorTareas.java
@@ -0,0 +1,13 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RastreadorTareas {
+    public static void main(String[] args) {
+        
+    }
+}
