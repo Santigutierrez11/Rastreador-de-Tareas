9a3ff76902d3e9100d651f23edb34fa5881af6f0
SantiagoGJ <santigutierrez0001@gmail.com>
2025-07-14 21:40:26 -0500

    Se agrega menu de opciones

--- src/main/java/RastreadorTareas.java
+++ src/main/java/RastreadorTareas.java
@@ -5,11 +5,20 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Date;
+import java.util.Scanner;
 
 public class RastreadorTareas {
     private static final String FILE_PATH = "tareas.json";
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int respuesta;
+
+        menu();
+        respuesta = Integer.parseInt(in.nextLine());
+        
+
+
         try {
             File file = new File(FILE_PATH);
             JSONObject json;
@@ -29,10 +38,20 @@ public class RastreadorTareas {
 
             try (FileWriter writer = new FileWriter(FILE_PATH)) {
                 writer.write(json.toString(4)); // Escribe el contenido del json en el archivo
-                System.out.println("Archivo creado");
+                System.out.println("Correcto");
             }
         } catch (IOException e){
             e.printStackTrace();
         }
+    }
+
+    public static void menu(){
+        System.out.println("**** Rastreador de Tareas ****");
+        System.out.println("1. Listar tareas");
+        System.out.println("2. Modificar tarea");
+        System.out.println("3. Crear tarea");
+        System.out.println("4. Eliminar tarea");
+        System.out.println("5. Salir");
+        System.out.print("\nSeleccione una opción: ");
     }
 }
