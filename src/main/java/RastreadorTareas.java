360bbc1a04ebbe42a95509e9c6855690bfbe5019
SantiagoGJ <santigutierrez0001@gmail.com>
2025-07-31 21:46:09 -0500

    Se importan las clases para menejo de archivos y json

--- src/main/java/RastreadorTareas.java
+++ src/main/java/RastreadorTareas.java
@@ -1,3 +1,11 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
 import java.util.Scanner;
 
 public class RastreadorTareas {
